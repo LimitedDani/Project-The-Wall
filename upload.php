f52dafd337ecfc73b2aa9430f989250d64a5887f
Traxx186 <justinvanderkruit@hotmail.com>
2017-04-11 12:56:48 +0200

    Naam aagepast

--- upload.php
+++ upload.php
@@ -55,7 +55,7 @@ $active = '';
                 <div class="col-md-9">
                     <div class="panel panel-info">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Upload new post</h3>
+                            <h3 class="panel-title">Upload nieuwe foto</h3>
                         </div>
                         <div class="panel-body">
                             <div class="row">
